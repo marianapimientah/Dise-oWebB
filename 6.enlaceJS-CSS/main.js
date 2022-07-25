b01d70f27d759587644700440f244aa21110c651
marianapimientah <marianapimientahernandez@outlook.com>
2022-07-24 20:24:25 -0500

    second commit

---
+++ 6.enlaceJS-CSS/main.js
@@ -0,0 +1,9 @@
+'use strict';
+//Los contenedores que posee JS
+var variable1 = 3; //Ámbito global 
+let variable2 = 4; //Ámbito local
+const variable3 = 1; //No se puede modificar
+
+console.log(variable1+variable2);
+
+//alert("El valor de la variable 2: " + variable2);
